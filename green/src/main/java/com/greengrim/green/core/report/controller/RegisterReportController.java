5faf9abca0ca2e64c915da107cc69c0eb5899ccf
bora <youonl5@naver.com>
2024-04-07 16:37:49 +0900

    Feat: 신고하기 API

--- green/src/main/java/com/greengrim/green/core/report/controller/RegisterReportController.java
+++ green/src/main/java/com/greengrim/green/core/report/controller/RegisterReportController.java
@@ -1,9 +1,37 @@
 package com.greengrim.green.core.report.controller;
 
+import com.greengrim.green.common.auth.CurrentMember;
+import com.greengrim.green.core.member.Member;
+import com.greengrim.green.core.report.ReportType;
+import com.greengrim.green.core.report.dto.ReportRequestDto.RegisterReport;
+import com.greengrim.green.core.report.service.RegisterReportService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class RegisterReportController {
+
+    private final RegisterReportService registerReportService;
+
+    /**
+     * [POST] 신고하기
+     * /visitor/report
+     */
+    @ResponseBody
+    @PostMapping("/visitor/report")
+    public ResponseEntity<Integer> registerReportMember(
+            @CurrentMember Member member,
+            @RequestParam(value = "type") ReportType type,
+            @RequestBody @Valid RegisterReport registerReport) {
+        registerReportService.registerReport(member, type, registerReport);
+        return new ResponseEntity<>(200, HttpStatus.OK);
+    }
 }
