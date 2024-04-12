4d4b33e1dc4f52e59371b3d31808fa034b26ecbb
bora <youonl5@naver.com>
2024-04-12 16:45:03 +0900

    Feat: Hiding entity 생성
    Member, Challenge, Certification, Nft

---
+++ green/src/main/java/com/greengrim/green/core/hiding/member/MemberHiding.java
@@ -0,0 +1,34 @@
+package com.greengrim.green.core.hiding.member;
+
+import com.greengrim.green.common.entity.BaseTime;
+import com.greengrim.green.core.member.Member;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class MemberHiding extends BaseTime {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private Long memberId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member hiddenMember;
+
+}
