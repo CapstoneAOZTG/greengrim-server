5aa4e2f9950a4de75e9adb2a45925d3f8bd881a8
bora <youonl5@naver.com>
2024-04-14 16:59:24 +0900

    Feat: Challenge 최소 인증 횟수 0회 허용

--- green/src/main/java/com/greengrim/green/core/challenge/Challenge.java
+++ green/src/main/java/com/greengrim/green/core/challenge/Challenge.java
@@ -46,7 +46,7 @@ public class Challenge extends BaseTime {
     @NotNull
     private int ticketCurrentCount; // 현재 남은 티켓 개수
     @NotNull
-    @Min(2)
+    @Min(0)
     @Max(6)
     private int weekMinCount;       // 주 최소 인증 횟수
     @NotNull
