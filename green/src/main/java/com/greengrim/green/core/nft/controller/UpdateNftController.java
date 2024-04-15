0c899cdbf13d6284eefca4f534afe888044e579b
Yeonsang <95534831+leeys1218@users.noreply.github.com>
2024-04-15 15:34:23 +0900

    refactor: [#104] NFT 수정, 삭제 API 구현

--- green/src/main/java/com/greengrim/green/core/nft/controller/UpdateNftController.java
+++ green/src/main/java/com/greengrim/green/core/nft/controller/UpdateNftController.java
@@ -2,27 +2,42 @@ package com.greengrim.green.core.nft.controller;
 
 import com.greengrim.green.common.auth.CurrentMember;
 import com.greengrim.green.core.member.Member;
-import com.greengrim.green.core.nft.service.UpdateNftService;
+import com.greengrim.green.core.nft.dto.NftRequestDto.NftModifyInfo;
+import com.greengrim.green.core.nft.dto.NftResponseDto.NftDetailInfo;
+import com.greengrim.green.core.nft.usecase.UpdateNftUseCase;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class UpdateNftController {
 
-    private final UpdateNftService updateNftService;
+    private final UpdateNftUseCase updateNftUseCase;
 
+    /**
+     * [DELETE] NFT 삭제하기
+     */
     @Operation(summary = "NFT 삭제하기")
-    @DeleteMapping("/member/nfts/{id}")
-    public ResponseEntity<Integer> deleteNft(
-        @CurrentMember Member member,
-        @PathVariable("id") Long id) {
-        updateNftService.delete(member, id);
+    @DeleteMapping("/visitor/nfts/{id}")
+    public ResponseEntity<Integer> deleteNft(@CurrentMember Member member, @PathVariable("id") Long id) {
+        updateNftUseCase.delete(member, id);
         return new ResponseEntity<>(200, HttpStatus.OK);
+    }
+
+    /**
+     * [PATCH] NFT 수정하기
+     */
+    @Operation(summary = "NFT 수정하기")
+    @PatchMapping("/visitor/nfts")
+    public ResponseEntity<NftDetailInfo> modifyNft(@CurrentMember Member member, @RequestBody NftModifyInfo modifyInfo) {
+        return new ResponseEntity<>(updateNftUseCase.modify(member, modifyInfo), HttpStatus.OK);
     }
 }
--- green/src/main/java/com/greengrim/green/core/nft/service/UpdateNftService.java
+++ green/src/main/java/com/greengrim/green/core/nft/service/UpdateNftService.java
@@ -4,7 +4,10 @@ import com.greengrim.green.common.exception.BaseException;
 import com.greengrim.green.common.exception.errorCode.NftErrorCode;
 import com.greengrim.green.core.member.Member;
 import com.greengrim.green.core.nft.Nft;
+import com.greengrim.green.core.nft.dto.NftRequestDto.NftModifyInfo;
+import com.greengrim.green.core.nft.dto.NftResponseDto.NftDetailInfo;
 import com.greengrim.green.core.nft.repository.NftRepository;
+import com.greengrim.green.core.nft.usecase.UpdateNftUseCase;
 import jakarta.transaction.Transactional;
 import java.util.Objects;
 import lombok.RequiredArgsConstructor;
@@ -13,20 +16,39 @@ import org.springframework.stereotype.Service;
 @Transactional
 @Service
 @RequiredArgsConstructor
-public class UpdateNftService {
+public class UpdateNftService implements UpdateNftUseCase {
 
     private final NftRepository nftRepository;
+    private final String[][] traits;
 
+    /**
+     * NFT 삭제하기
+     */
     @Transactional
     public void delete(Member member, Long id) {
         Nft nft = nftRepository.findByIdAndStatusTrue(id)
             .orElseThrow(() -> new BaseException(NftErrorCode.EMPTY_NFT));
+
         checkIsMine(member.getId(), nft.getMember().getId());
 
         nft.delete();
     }
 
-    private void checkIsMine(Long viewerId, Long ownerId) {
+    /**
+     * NFT 수정하기
+     */
+    @Transactional
+    public NftDetailInfo modify(Member member, NftModifyInfo modifyInfo) {
+        Nft nft = nftRepository.findByIdAndStatusTrue(modifyInfo.getId())
+            .orElseThrow(() -> new BaseException(NftErrorCode.EMPTY_NFT));
+
+        checkIsMine(member.getId(), nft.getMember().getId());
+
+        nft.modify(modifyInfo);
+        return new NftDetailInfo(nft, true, traits);
+    }
+
+    public void checkIsMine(Long viewerId, Long ownerId) {
         if(!Objects.equals(viewerId, ownerId)) {
             throw new BaseException(NftErrorCode.NO_AUTHORIZATION);
         }
