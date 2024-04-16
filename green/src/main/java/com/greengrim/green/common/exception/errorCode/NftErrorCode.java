37822fd5471dc6b73dc391dc25e9b64284c7938c
Yeonsang <95534831+leeys1218@users.noreply.github.com>
2024-04-16 23:54:01 +0900

    refactor: [#104] NFT 에러코드 추가

--- green/src/main/java/com/greengrim/green/common/exception/errorCode/NftErrorCode.java
+++ green/src/main/java/com/greengrim/green/common/exception/errorCode/NftErrorCode.java
@@ -10,7 +10,9 @@ import org.springframework.http.HttpStatus;
 public enum NftErrorCode implements ErrorCode {
     INVALID_NFT("NFT_001", "유효하지 않은 NFT입니다.", HttpStatus.CONFLICT),
     EMPTY_NFT("NFT_002", "교환 가능한 NFT의 개수가 부족합니다.", HttpStatus.CONFLICT),
-    NO_AUTHORIZATION("NFT_003", "NFT에 대해 권한이 없습니다.", HttpStatus.CONFLICT)
+    ALREADY_EXCHANGED_NFT("NFT_003", "이미 교환된 NFT 입니다.", HttpStatus.CONFLICT),
+    NO_AUTHORIZATION("NFT_004", "NFT에 대해 권한이 없습니다.", HttpStatus.CONFLICT),
+    EXCHANGE_FAIL("NFT_005", "교환에 싪패했습니다.", HttpStatus.CONFLICT)
     ;
     private final String errorCode;
     private final String message;
