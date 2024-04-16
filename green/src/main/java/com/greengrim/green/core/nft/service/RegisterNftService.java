8871fbfb6e27096d176a6c64609d2c2491ce9187
Yeonsang <95534831+leeys1218@users.noreply.github.com>
2024-04-16 23:55:06 +0900

    refactor: [#104] NFT 교환하기 작성

--- green/src/main/java/com/greengrim/green/core/nft/service/RegisterNftService.java
+++ green/src/main/java/com/greengrim/green/core/nft/service/RegisterNftService.java
@@ -1,37 +1,60 @@
 package com.greengrim.green.core.nft.service;
 
+import com.greengrim.green.common.exception.BaseException;
+import com.greengrim.green.common.exception.errorCode.NftErrorCode;
+import com.greengrim.green.common.fcm.FcmService;
+import com.greengrim.green.common.web3j.Abi;
 import com.greengrim.green.core.member.Member;
+import com.greengrim.green.core.member.repository.MemberRepository;
 import com.greengrim.green.core.nft.Nft;
-import com.greengrim.green.core.nft.dto.NftRequestDto.RegisterNft;
 import com.greengrim.green.core.nft.repository.NftRepository;
+import com.greengrim.green.core.nft.usecase.RegisterNftUseCase;
 import jakarta.transaction.Transactional;
+import java.math.BigInteger;
+import java.util.concurrent.ExecutionException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.web3j.protocol.core.methods.response.TransactionReceipt;
 
-@Transactional
 @Log4j2
 @Service
 @RequiredArgsConstructor
-public class RegisterNftService {
+public class RegisterNftService implements RegisterNftUseCase {
 
     private final NftRepository nftRepository;
+    private final MemberRepository memberRepository;
+    private final FcmService fcmService;
+    private final Abi contract;
 
-    public Nft register(Member member, RegisterNft registerNft, int tokenId,
-        String txHash, String imgUrl) {
-        Nft nft = Nft.builder()
-            .tokenId(tokenId)
-            .txHash(txHash)
-            .imgUrl(imgUrl)
-            .title(registerNft.getTitle())
-            .description(registerNft.getDescription())
-            .reportCount(0)
-            .status(true)
-            .member(member)
-            .build();
-        nftRepository.save(nft);
-        return nft;
+    /**
+     * NFT 교환하기
+     */
+    @Async
+    @Transactional
+    public void exchangeNft(Member member, Long id) {
+        Nft nft = nftRepository.findByIdAndStatusTrue(id)
+            .orElseThrow(() -> new BaseException(NftErrorCode.EMPTY_NFT));
+
+        if(nft.getMember() != null) {
+            throw new BaseException(NftErrorCode.ALREADY_EXCHANGED_NFT);}
+
+        TransactionReceipt transactionReceipt = null;
+        try {
+            transactionReceipt = contract.safeMint("0x3a475978c5D12d07c938350a4D83CDed70Df73CF", BigInteger.valueOf(5)).sendAsync().get();
+        }
+        catch (InterruptedException | ExecutionException e) {
+            throw new BaseException(NftErrorCode.EXCHANGE_FAIL);}
+
+        if (transactionReceipt != null) {
+            member.minusPoint(300);
+            nft.setMember(member);
+            memberRepository.save(member);
+            fcmService.sendMintingSuccess(member, nft.getId());
+        }
+        else {
+            fcmService.sendMintingFail(member);
+        }
     }
-
-
 }
---
+++ green/src/main/java/com/greengrim/green/core/nft/usecase/RegisterNftUseCase.java
@@ -0,0 +1,9 @@
+package com.greengrim.green.core.nft.usecase;
+
+import com.greengrim.green.core.member.Member;
+
+public interface RegisterNftUseCase {
+
+    void exchangeNft(Member member, Long id);
+
+}
