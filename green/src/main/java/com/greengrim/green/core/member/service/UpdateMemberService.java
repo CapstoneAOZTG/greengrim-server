60f09138dde9c5e1f2969aae07d78b52bcfed53a
Yeonsang <95534831+leeys1218@users.noreply.github.com>
2024-04-24 18:49:21 +0900

    feature: [#110] 채팅 매세지 삭제 추가

--- green/src/main/java/com/greengrim/green/core/member/service/UpdateMemberService.java
+++ green/src/main/java/com/greengrim/green/core/member/service/UpdateMemberService.java
@@ -4,10 +4,13 @@ package com.greengrim.green.core.member.service;
 import com.greengrim.green.common.fcm.FcmService;
 import com.greengrim.green.common.oauth.jwt.JwtTokenProvider;
 import com.greengrim.green.common.s3.S3Service;
+import com.greengrim.green.core.certification.service.UpdateCertificationService;
+import com.greengrim.green.core.chat.service.ChatService;
 import com.greengrim.green.core.member.Member;
 import com.greengrim.green.core.member.dto.MemberRequestDto.ModifyProfile;
 import com.greengrim.green.core.member.dto.MemberResponseDto;
 import com.greengrim.green.core.member.repository.MemberRepository;
+import com.greengrim.green.core.nft.service.UpdateNftService;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -20,19 +23,23 @@ public class UpdateMemberService  {
     private final JwtTokenProvider jwtTokenProvider;
     private final MemberRepository memberRepository;
 
+    private final UpdateCertificationService updateCertificationService;
+    private final UpdateNftService updateNftService;
+    private final ChatService chatService;
+
     private final FcmService fcmService;
     private final S3Service s3Service;
 
     public MemberResponseDto.TokenInfo refreshAccessToken(Member member) {
         MemberResponseDto.TokenInfo newTokenInfo
-                = jwtTokenProvider.generateToken(member.getId());
+            = jwtTokenProvider.generateToken(member.getId());
 
         // DB refreshToken 변경
         member.changeRefreshToken(newTokenInfo.getRefreshToken());
         memberRepository.save(member);
 
         return new MemberResponseDto.TokenInfo(
-                newTokenInfo.getAccessToken(), newTokenInfo.getRefreshToken(), member.getId());
+            newTokenInfo.getAccessToken(), newTokenInfo.getRefreshToken(), member.getId());
     }
 
     public void modifyProfile(Member member, ModifyProfile modifyProfile) {
@@ -43,15 +50,20 @@ public class UpdateMemberService  {
 
         // Member 엔티티 업로드
         member.modifyMember(modifyProfile.getNickName(),
-                modifyProfile.getIntroduction(),
-                modifyProfile.getProfileImgUrl());
+            modifyProfile.getIntroduction(),
+            modifyProfile.getProfileImgUrl());
         memberRepository.save(member);
     }
 
     public void deleteMember(Member member) {
-        // member.setStatusFalse();
-        // TODO: 삭제된 member와 관련된 모든 리소스 삭제
-        memberRepository.delete(member);
+        // 인증 soft delete
+        updateCertificationService.setCertificationStatusFalseByMember(member);
+        // NFT soft delete
+        updateNftService.setCertificationStatusFalseByMember(member);
+        // 채팅 메세지 delete
+        chatService.deleteMember(member);
+        // 멤버 soft delete
+        member.setStatusFalse();
     }
 
     public void plusPoint(Member member) {
