f2dc74efa1a95a99cffabefa36d4ad7f90dbe551
bora <youonl5@naver.com>
2024-05-12 17:35:15 +0900

    Fix: Dto @NotBlank
    Long은 @NotNull로 validate

--- green/src/main/java/com/greengrim/green/core/nft/dto/NftRequestDto.java
+++ green/src/main/java/com/greengrim/green/core/nft/dto/NftRequestDto.java
@@ -1,6 +1,7 @@
 package com.greengrim.green.core.nft.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,7 +12,7 @@ public class NftRequestDto {
     @NoArgsConstructor
     @AllArgsConstructor
     public static class NftModifyInfo {
-        @NotBlank(message = "그림 ID는 빈 값일 수 없습니다.")
+        @NotNull(message = "NFT ID는 빈 값일 수 없습니다.")
         private Long id;
         @NotBlank(message = "제목은 빈 값일 수 없습니다.")
         private String title;
