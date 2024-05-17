2b56082f93edc36ede66fbfaf817526fc577e76e
유지수 <71329329+YooJisu826@users.noreply.github.com>
2024-05-17 20:13:44 +0900

    AOP 로깅 구현 (#153)
    * Fix: 홈 화면 핫 챌린지 API
    
    리소스 없어도 Exception 발생하지 않도록
    
    * Fix: 상호 인증 flag 수정
    
    내 인증인 경우 VerificationFlag.DEACTIVATION
    
    * Chore: [#144] 로깅 build.gradle 설정
    
    * Infra: [#144] Interceptor & LogAspect 구현
    
    * Infra: [#144] WebConfig 설정을 통한 Interceptor 활성화
    
    * Infra: [#144] BaseException 로깅 추가
    
    * Infra: [#144] @LogExecutionTime 구현
    
    API의 실행 시간을 체크하는 어노테이션
    
    * Infra: [#144] API 요청한 memberId 로깅 추가
    
    * Docs: [#144] 로깅 설정 파일

---
+++ green/src/main/java/com/greengrim/green/common/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.greengrim.green.common.config;
+
+import com.greengrim.green.common.log.Interceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    private final Interceptor interceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(interceptor)
+                .addPathPatterns("/**");    // 모든 Request 에 대해
+    }
+}
--- green/src/main/java/com/greengrim/green/common/oauth/jwt/JwtAuthenticationFilter.java
+++ green/src/main/java/com/greengrim/green/common/oauth/jwt/JwtAuthenticationFilter.java
@@ -6,12 +6,14 @@ import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.GenericFilterBean;
 
 import java.io.IOException;
 
+@Log4j2
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends GenericFilterBean {
 
@@ -45,6 +47,7 @@ public class JwtAuthenticationFilter extends GenericFilterBean {
                 jwtTokenProvider.setBlackList(token);
             }
         }
+        log.info("Request MemberId: {}", jwtTokenProvider.getMemberIdByAccessToken(token));
         SecurityContextHolder.getContext().setAuthentication(authentication);
         chain.doFilter(request, response);
 
