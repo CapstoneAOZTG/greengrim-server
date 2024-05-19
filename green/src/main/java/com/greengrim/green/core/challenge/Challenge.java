e1cac613a0d889c5b77823d5a4fd66187413ff34
bora <youonl5@naver.com>
2024-05-19 20:32:05 +0900

    Refactor: 챌린지 태그 기능 변경

--- green/src/main/java/com/greengrim/green/core/challenge/Challenge.java
+++ green/src/main/java/com/greengrim/green/core/challenge/Challenge.java
@@ -11,7 +11,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
 import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
@@ -42,14 +41,6 @@ public class Challenge extends BaseTime {
     @NotNull
     private int goalCount;          // 목표 인증 횟수
     @NotNull
-    private int ticketTotalCount;   // 총 티켓 개수
-    @NotNull
-    private int ticketCurrentCount; // 현재 남은 티켓 개수
-    @NotNull
-    @Min(0)
-    @Max(6)
-    private int weekMinCount;       // 주 최소 인증 횟수
-    @NotNull
     @Max(100)
     private int capacity;           // 수용 가능 인원
     @NotNull
@@ -71,27 +62,12 @@ public class Challenge extends BaseTime {
         return "인증 " + this.getGoalCount() + "회";
     }
 
-    public String getTicketCountTag() {
-        return "티켓 " + (this.ticketTotalCount - this.ticketCurrentCount)
-                + "/" + this.ticketTotalCount;
-    }
-
-    public String getWeekMinCountTag() {
-        return "최소 인증 주 " + this.weekMinCount + "회";
-    }
-
     public String getParticipantCountTag() {
         return "인원 " + this.headCount + "/" + this.capacity;
     }
 
     public void setHeadCount(int headCount) {
         this.headCount = headCount;
-    }
-
-    public void minusTicketCurrentCount() {
-        if(this.ticketCurrentCount != 0) {
-            this.ticketCurrentCount--;
-        }
     }
 
     public void plusReportCount() {
