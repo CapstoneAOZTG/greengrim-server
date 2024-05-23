f49cbe89033c82dde3c400b19fcab4c7d27fcb3f
bora <youonl5@naver.com>
2024-05-23 19:50:47 +0900

    Style: 불필요한 import 제거

--- green/src/main/java/com/greengrim/green/core/notice/service/RegisterNoticeService.java
+++ green/src/main/java/com/greengrim/green/core/notice/service/RegisterNoticeService.java
@@ -1,10 +1,6 @@
 package com.greengrim.green.core.notice.service;
 
-import com.greengrim.green.common.exception.BaseException;
-import com.greengrim.green.common.exception.errorCode.MemberErrorCode;
 import com.greengrim.green.common.fcm.FcmService;
-import com.greengrim.green.core.member.Member;
-import com.greengrim.green.core.member.Role;
 import com.greengrim.green.core.notice.Notice;
 import com.greengrim.green.core.notice.dto.NoticeRequestDto.RegisterNotice;
 import com.greengrim.green.core.notice.dto.NoticeResponseDto.NoticeDetailInfo;
